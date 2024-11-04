408c29174a434f7ad20326778ef186dfc83660c5
MykhailoRospopchuk <rosmisha@gmail.com>
2024-11-04 16:55:17 +0200

    first one

---
+++ quickstart/src/IdentityServer/Config.cs
@@ -0,0 +1,41 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+using IdentityModel;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        { 
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new ApiScope(name: "api1", displayName: "My API")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new Client
+            {
+                ClientId = "client",
+                
+                // machine to machine
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                
+                // secret for authorization
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+                
+                // define list of scopes that this client cat access
+                AllowedScopes = { "api1" }
+            }
+        };
+}
