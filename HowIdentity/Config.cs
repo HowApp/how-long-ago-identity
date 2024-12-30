e445af1fcbc0bb5306f928b97158b44c173a479e
mykhailo_u <rosmisha@gmail.com>
2024-12-30 20:54:59 +0200

    upd

--- HowIdentity/Config.cs
+++ HowIdentity/Config.cs
@@ -1,5 +1,6 @@
 ﻿namespace HowIdentity;
 
+using Duende.IdentityServer;
 using Duende.IdentityServer.Models;
 
 public static class Config
@@ -14,39 +15,31 @@ public static class Config
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
         {
-            new ApiScope("scope1"),
-            new ApiScope("scope2"),
+            new ApiScope("admin"),
+            new ApiScope("user")
         };
 
     public static IEnumerable<Client> Clients =>
         new Client[]
         {
-            // m2m client credentials flow client
-            new Client
-            {
-                ClientId = "m2m.client",
-                ClientName = "Client Credentials Client",
-
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                AllowedScopes = { "scope1" }
-            },
-
             // interactive client using code flow + pkce
             new Client
             {
-                ClientId = "interactive",
+                ClientId = "how-web-app",
                 ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
 
                 AllowedGrantTypes = GrantTypes.Code,
 
-                RedirectUris = { "https://localhost:44300/signin-oidc" },
-                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+                RedirectUris = { "https://localhost:7560/signin-oidc" },
+                FrontChannelLogoutUri = "https://localhost:7560/signout-oidc",
+                PostLogoutRedirectUris = { "https://localhost:7560/signout-callback-oidc" },
 
                 AllowOfflineAccess = true,
-                AllowedScopes = { "openid", "profile", "scope2" }
+                AllowedScopes = { 
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile, 
+                    "admin",
+                    "user" }
             },
         };
 }
