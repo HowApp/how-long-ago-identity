b220562bcee2a7a2d3e3bd70c7feed947479c5f0
mykhailo_u <rosmisha@gmail.com>
2025-01-04 23:14:44 +0200

    add http client ot blazor

--- learn/BlazorTest/Program.cs
+++ learn/BlazorTest/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorTest;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,6 +9,25 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var host = builder.HostEnvironment.BaseAddress;
 
+builder.Services.AddHttpClient("api-test-client.authorized", client => 
+    client.BaseAddress = new Uri("https://localhost:7088"))
+    .AddHttpMessageHandler(httpBuilder =>
+    {
+        var handler = httpBuilder.GetService<AuthorizationMessageHandler>()!
+            .ConfigureHandler(
+                authorizedUrls: new[] { "https://localhost:7088" },
+                scopes: new[] { "scope.api-test" });
+        
+        return handler;
+    });
+
+builder.Services.AddHttpClient("api-test-client.unauthorized", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7088");
+});
+
+builder.Services.AddScoped(sp =>  sp.GetService<IHttpClientFactory>()!.CreateClient("api-test-client.authorized"));
+builder.Services.AddScoped(sp =>  sp.GetService<IHttpClientFactory>()!.CreateClient("api-test-client.unauthorized"));
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddOidcAuthentication(options =>
