393c76979c5fe0b0943914cbb07374188d93ec2f
MykhailoRospopchuk <rosmisha@gmail.com>
2025-01-14 15:05:07 +0200

    upd

--- learn/BlazorTest/Program.cs
+++ learn/BlazorTest/Program.cs
@@ -29,10 +29,12 @@ builder.Services.AddScoped(sp =>  sp.GetService<IHttpClientFactory>()!.CreateCli
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddOidcAuthentication(options =>
-{
-    // Configure your authentication provider options here.
-    // For more information, see https://aka.ms/blazor-standalone-auth
-    builder.Configuration.Bind("oidc", options.ProviderOptions);
-});
+    {
+        // Configure your authentication provider options here.
+        // For more information, see https://aka.ms/blazor-standalone-auth
+        builder.Configuration.Bind("oidc", options.ProviderOptions);
+        options.UserOptions.RoleClaim = "role"; 
+    })
+    .AddAccountClaimsPrincipalFactory<CustomAccountClaimsPrincipalFactory<RemoteUserAccount>>();
 
 await builder.Build().RunAsync();
